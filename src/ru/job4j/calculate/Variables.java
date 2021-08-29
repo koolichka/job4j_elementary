fd71d0a07156078356b08d5cb634a5b4475fb4fe
koolichka@mail.ru <QFR-svw-kTD-scP>
2021-08-29 15:16:40 +0300

    3. Арифметические операции. [#185]

---
+++ src/ru/job4j/calculate/Variables.java
@@ -0,0 +1,13 @@
+package ru.job4j.calculate;
+
+public class Variables {
+    public static void main(String[] args) {
+        int age = 18;
+        System.out.println(age);
+        double length = 3.940708006258929e+69;
+        System.out.println(length);
+        boolean baby = true;
+        System.out.println(baby);
+
+    }
+}
