a6f718620349a196012a481051d4671d73124dfc
koolichka@mail.ru <QFR-svw-kTD-scP>
2021-08-31 18:34:06 +0300

    4.2. Результат работы метода [#232622]

--- src/ru/job4j/calculator/Calculator.java
+++ src/ru/job4j/calculator/Calculator.java
@@ -1,19 +1,36 @@
 package ru.job4j.calculator;
 
+import java.util.concurrent.Callable;
+
 public class Calculator {
+
+    public static void plus(int first, int second) {
+        int result = first + second;
+        System.out.println(result);
+    }
+
     public static void main(String[] args) {
-        int one = 1;
-        int two = 2;
-        int four = 4;
-        int five = 5;
-        int six = 6;
-        int onePlusTwo = one + two;
-        int sixDivTwo = six / two;
-        int fiveMinusTwo = five - two;
-        int fourTimeTwo = four * two;
-        System.out.println(onePlusTwo);
-        System.out.println(sixDivTwo);
-        System.out.println(fiveMinusTwo);
-        System.out.println(fourTimeTwo);
+        Calculator.plus(100, 500);
+        Calculator.plus(4, 2);
+        Calculator.plus(3, 5);
     }
 }
+
+
+/*
+ *//*int one = 1;
+        int two = 2;
+        int result = one + two;
+        System.out.println(result);
+        one = 10;
+        two = 11;
+        result = one + two;
+        System.out.println(result);*//*
+
+
+ *//*int size = 10;
+        size = size + 100;
+        size = size - 5;
+        System.out.println(size);*//*
+    }
+}*/
