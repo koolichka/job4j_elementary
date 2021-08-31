b322a7e190586a8eb2c9ad2e867a64e28453a296
koolichka@mail.ru <QFR-svw-kTD-scP>
2021-08-31 18:59:25 +0300

    5. Конвертер валюты [#41583]

---
+++ src/ru/job4j/converter/Converter.java
@@ -0,0 +1,33 @@
+package ru.job4j.converter;
+
+public class Converter {
+    public static float rubleToEuro(float value) {
+        float rsl = value / 70;
+        return rsl;
+    }
+    public static float euroToRuble(float value) {
+        float rsl = value * 70;
+        return rsl;
+    }
+
+    public static float rubleToDollar(float value){
+        float rsl = value / 60; //формула перевода рубли в доллары
+        return rsl;
+    }
+    public static float dollarToRuble(float value){
+        float rsl = value * 60; //формула перевода рубли в доллары
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        float euro = Converter.rubleToEuro(140);
+        System.out.println("140 rubles are " + euro + " euro.");
+        float dollar = Converter.rubleToDollar(140);
+        System.out.println("140 rubles are " + dollar + " dollar.");
+        float rubleE = Converter.euroToRuble(2);
+        System.out.println("2 euro are  " + rubleE + " rubles");
+        float rubleD = Converter.dollarToRuble(2);
+        System.out.println("2 dollar are  " + rubleD + " rubles");
+
+    }
+}
