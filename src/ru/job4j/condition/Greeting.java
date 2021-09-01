0b1a2bf7972d3e784b324bcef63a7407359dacd3
koolichka@mail.ru <QFR-svw-kTD-scP>
2021-09-01 20:41:07 +0300

    9.1. String [#237407]

---
+++ src/ru/job4j/condition/Greeting.java
@@ -0,0 +1,13 @@
+package ru.job4j.condition;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "I like Java!";
+        String separate = " ";
+        System.out.println(idea);
+        System.out.println(idea + separate + "But I am newbie!");
+        int year = 2021;
+        idea = idea + separate + year;
+        System.out.println(idea);
+    }
+}
