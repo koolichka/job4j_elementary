c6e74b51bf2a7de5c038939eaa2c8938a0417b69
koolichka@mail.ru <QFR-svw-kTD-scP>
2021-09-06 11:30:43 +0300

    0. Что такое тестирование [#159431]

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -22,14 +22,27 @@ public class Converter {
     }
 
     public static void main(String[] args) {
-        float euro = Converter.rubleToEuro(140);
-        System.out.println("140 rubles are " + euro + " euro.");
-        float dollar = Converter.rubleToDollar(140);
-        System.out.println("140 rubles are " + dollar + " dollar.");
-        float rubleE = Converter.euroToRuble(2);
-        System.out.println("2 euro are  " + rubleE + " rubles");
-        float rubleD = Converter.dollarToRuble(2);
-        System.out.println("2 dollar are  " + rubleD + " rubles");
+        float in = 140;
+        float inR = 2;
+        float expected = 2;
+        float out = Converter.rubleToEuro(in);
+        boolean passed = expected == out;
+        System.out.println("140 rubles are 2 euro. Test result : " + passed);
+
+        float expectedRTD = 2.3333333F;
+        float outRTD = Converter.rubleToDollar(in);
+        boolean passedRTD = expectedRTD == outRTD;
+        System.out.println("140 rubles are 2.3333333 dollar. Test result : " + passed);
+
+        float expectedETR = 140;
+        float outETR = Converter.euroToRuble(inR);
+        boolean passedETR = expectedETR == outETR;
+        System.out.println("2 euro are  140 rubles. Test result : " + passedETR);
+
+        float expectedDTR = 120;
+        float outDTR = Converter.dollarToRuble(inR);
+        boolean passedDTR = expectedDTR == outDTR;
+        System.out.println("2 dollar are  120 rubles. Test result : " + passedDTR);
 
     }
 }
