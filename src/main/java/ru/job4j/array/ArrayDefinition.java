1b756be888a85e396d14ef9bd783c3143288e767
koolichka@mail.ru <QFR-svw-kTD-scP>
2021-10-25 19:10:11 +0300

    6.0.2. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -9,5 +9,15 @@ public class ArrayDefinition {
         System.out.println("Размер массива равен " + surnames.length);
         float[] prices = new float[40];
         System.out.println("Размер массива равен " + prices.length);
+
+        String[] names = new String[4];
+        names[0] = "Petr Arsentev";
+        names[1] = "Andrei Kulikov";
+        names[2] = "Curt Cobain";
+        names[3] = "Takeshi Kitana";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
